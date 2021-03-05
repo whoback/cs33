03bd06755666a4a2ae81abc7857b01ebb20af011
Will Hoback <will.hoback@gmail.com>
2021-03-05 17:02:55 -0600

    more models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
 
 class User(AbstractUser):
@@ -7,13 +8,16 @@ class User(AbstractUser):
 
 
 class Listing(models.Model):
-    title = models.CharField
-    description = models.TextField
+    title = models.CharField(max_length=255)
+    description = models.TextField()
     starting_bid = models.DecimalField(max_digits=6, decimal_places=2)
     current_bid = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField
-    date_listed = models.DateTimeField
+    image_url = models.URLField()
+    date_listed = models.DateTimeField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    listed_by = models.CharField(max_length=255)
 
 
 class Comment(models.Model):
-    pass
+    comment_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
