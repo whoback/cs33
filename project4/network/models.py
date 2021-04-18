bd39ebb68887e9623c6f4c7325523cf6f26237e6
Will Hoback <will.hoback@gmail.com>
2021-04-18 14:56:09 -0500

    add post class

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.CharField(max_length=500, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    like = models.ManyToManyField(
+        User,  blank=True, related_name="liked_user")
+
+    def __str__(self):
+        return "{self.user.username} -> {self.post}"
